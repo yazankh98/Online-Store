8e94895def0620424778744498aa8d979c5ff91d
yazankh98 <yazan.kh.anam@gmail.com>
2025-12-06 21:10:32 +0400

    one

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
+
 })
